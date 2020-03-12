68de78041768f7990aa1a0c9fd1efd2997f23480
dentizane <dentizane@gmail.com>
2020-03-12 09:25:12 -0700

    Get-Date

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,'../public')));
+
+let count = 0;
+
+io.on('connection', (socket) => {
+    console.log('connection established');
+
+    socket.emit('welcome', `Hello user${count}`);
+    count++;
+    // socket.on('connect', () => {
+    //     count++;
+    //     io.emit('connect', () => {
+            
+    //     })
+    // })
+})
+
+server.listen(port, () => {
+    console.log(`Server up on port ${port}`);
+})
