42f54f36db24d23c0db3db06e3be71fed4328908
ZaneH <zane@dentimax.com>
2020-03-24 11:30:42 -0700

    Get-Date

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,11 +3,15 @@ const socket = io();
 const $form = document.querySelector('#message-form');
 const $input = document.querySelector('input');
 const $button = document.querySelector('button');
+const $roomTitle = document.querySelector('h2');
+const $sidebarUsers = document.querySelector('#sidebar-users');
 const $messages = document.querySelector('#messages');
 
 const tplMessage = document.querySelector('#tplMessage').innerHTML;
 
 const {username, room} = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+$roomTitle.innerHTML = room;
 
 socket.on('message', (message) => {
     console.log(message);
@@ -18,6 +22,10 @@ socket.on('message', (message) => {
     });
     $messages.insertAdjacentHTML('beforeend', html);
 });
+
+socket.on('userList', (userList) => {
+    $sidebarUsers.innerHTML = userList;
+})
 
 $form.addEventListener('submit', (e) => {
     e.preventDefault();
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketio = require('socket.io');
+const chalk = require('chalk');
 const { generateMessage } = require('./utils/message');
 
 const app = express();
@@ -18,32 +19,40 @@ io.on('connection', (socket) => {
     socket.on('join', ({ username, room }) => {
         socket.join(room);
         socket.username = username;
-
+        //console.log(chalk.blue(room));
         var strUsers = '';
-        const socks = Object.keys(io.sockets.adapter.rooms[room].sockets)
-        socks.forEach(socketid => strUsers = strUsers + io.sockets.sockets[socketid].username + '\r\n')
-        console.log(strUsers)
+        const socks = Object.keys(io.sockets.adapter.rooms[room].sockets);
+        socks.forEach(socketid => strUsers = strUsers + io.sockets.sockets[socketid].username + '</br>');
+        io.to(room).emit('userList', strUsers);
 
         socket.emit('message', generateMessage('Admin',`Welcome ${username}`));
         socket.broadcast.to(room).emit('message', generateMessage('Admin',`${username} has joined`));
     });
 
     socket.on('sendMessage', (message,callback) => {
-        console.log(Object.keys(socket.rooms)[0])
+        console.log(Object.keys(socket.rooms)[0]);
         io.to(Object.keys(socket.rooms)[0]).emit('message', generateMessage(socket.username,message));
         callback('Got it!');
     });
 
+    // preserve rooms after disconnect
+    socket.on('disconnecting', () => {
+        socket.rms = socket.rooms;
+    })
+
     socket.on('disconnect', () => {
-        const room = Object.keys(socket.rooms)[0];
-        io.to(room).emit('message','User disconnected');
+        const room = Object.keys(socket.rms)[0];
+        //console.log(chalk.red(JSON.stringify(socket.rms)))
+        //console.log(chalk.red(room))
+        io.to(room).emit('message',generateMessage('Admin',`${socket.username} has left`));
         socket.leave(room);
 
-        var strUsers = '';
-        const socks = Object.keys(io.sockets.adapter.rooms[room].sockets)
-        socks.forEach(socketid => strUsers = strUsers + io.sockets.sockets[socketid].username + '\r\n')
-        console.log(strUsers)
-        io.to(room).emit('userList',strUsers);
+        if (io.sockets.adapter.rooms[room]) {
+            var strUsers = '';
+            const socks = Object.keys(io.sockets.adapter.rooms[room].sockets);
+            socks.forEach(socketid => strUsers = strUsers + io.sockets.sockets[socketid].username + '</br>');
+            io.to(room).emit('userList', strUsers);
+        }
     });
 });
 
